9aa0d449cc8ae5dc72c1355d340fa51a96eabae0
mariolivera <mariooliveiratec@hotmail.com>
2022-10-01 10:57:37 -0300

    criado eventos com a const atualizarLista

--- eventos.js
+++ eventos.js
@@ -1,5 +1,8 @@
-function atualizarlista(){
-    fetch ('http://localhost:8001/compras')
-    .then()
-    .then();
-}
+const API_URL = 'http://localhost:666'
+    function atualizarLista() {      
+        tabela_lista.innerHTML ='',
+        fetch(API_URL+'telefones')
+        .then(function(resposta){
+            return resposta.json();
+        })
+    }
