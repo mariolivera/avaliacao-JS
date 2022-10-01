9dc959b20756d9fa92be5bcfe0720162a9f7b626
mariolivera <mariooliveiratec@hotmail.com>
2022-10-01 11:46:46 -0300

    finalizado fuction atualizarlista

--- eventos.js
+++ eventos.js
@@ -2,7 +2,26 @@ const API_URL = 'http://localhost:666'
     function atualizarLista() {      
         tabela_lista.innerHTML ='',
         fetch(API_URL+'telefones')
-        .then(function(resposta){
+        .then((resposta)=>{
             return resposta.json();
+        })
+        .then((lista)=>{
+            lista.forEach((cadacontato)=> {
+                tabela_lista.innerHTML +=`
+                    <tr>
+                        <td>${cadacontato.id}</td>
+                        <td>${cadacontato.nome}</td>
+                        <td>${cadacontato.telefone}</td>
+                        <td>${cadacontato.cidade}</td>
+                        <td>
+                            <button onclick = "contato(${cadacontato.id})" class="btn btn-outline-warning" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar">
+                                Editar
+                            </button>
+                            <button onclick="excluir(${cadacontato.id})" class="btn btn btn-outline-danger">
+                                Excluir
+                            </button>
+                        </td>
+                    </tr>`
+            });
         })
     }
