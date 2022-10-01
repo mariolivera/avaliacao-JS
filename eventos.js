8122f71a73452ca9b3aac96be20528edf8d351f3
mariolivera <mariooliveiratec@hotmail.com>
2022-10-01 20:21:35 -0300

    feito função criar, ver problema do Headers

--- eventos.js
+++ eventos.js
@@ -14,14 +14,55 @@ const API_URL = 'http://localhost:666'
                         <td>${cadacontato.telefone}</td>
                         <td>${cadacontato.cidade}</td>
                         <td>
-                            <button onclick = "contato(${cadacontato.id})" class="btn btn-outline-warning" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar">
+                            <button onclick: "contato(${cadacontato.id})" class="btn btn-outline-warning" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar">
                                 Editar
                             </button>
-                            <button onclick="excluir(${cadacontato.id})" class="btn btn btn-outline-danger">
+                            <button onclick:"excluir(${cadacontato.id})" class="btn btn btn-outline-danger">
                                 Excluir
                             </button>
                         </td>
                     </tr>`
             });
         })
+    }
+    function excluir(id) {
+        let resposta = confirm('Você perderar os dados do item deletado, tem certeza que quer excluir?');
+        if(resposta !== true){
+            return;
+        }
+    
+        fetch(API_URL+'/telefones/'+id,{
+            method: 'DELETE',
+        });
+        atualizarLista();
+    }
+    
+    function criar(){
+        event.preventDefault();
+        let contato = {
+            nome : document.getElementById('input_nome').Value,
+            numero : document.getElementById('input_telefone').Value,
+            cidade : document.getElementById('input_cidade').Value,
+        }
+        if(contato.nome=="" && contato.telefone=="" && contato.cidade==""){
+            alert('Dados invalidos');
+            return;
+        }
+        fetch(API_URL+'/telefones',{
+            method:'POST',
+            body:JSON.stringify(contato)
+            headers: {
+                'content-type': 'application/json',
+            },
+        })
+            .then((resposta)=>resposta.json())
+            .then((data)=> {
+                atualizarLista();
+                alert('Contato Atualizado')
+                console.log('success:',data);
+            })
+            .catch((erro)=>{
+                console.error('error:',erro);
+            });
+            formAdd.reset()
     }
